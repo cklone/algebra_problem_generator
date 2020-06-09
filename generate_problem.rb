178268eca90f0ff9af6c7d518dd129b2f3f92061
Chris Kline <chris@traveljoy.com>
2020-06-09 11:49:11 -0700

    Basic outline of a problem

---
+++ generate_problem.rb
@@ -0,0 +1,11 @@
+# Start with the answer
+# x = 4
+
+# Now, come up with a simple addition/subtraction algebra problem
+# x + y = z where x = 4
+# x = z - y so what could z and y be to make x 4?
+# 12 - 8 = 4, so z = 12 and y = 8, plug it in to the equation, and the problem is:
+# x + 8 = 12
+
+puts "Problem: x + 8 = 12"
+puts "Answer:  x = 4"
