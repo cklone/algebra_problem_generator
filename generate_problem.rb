82d978c2a0642e4694f0cb5830b30206147d4548
Chris Kline <chris@traveljoy.com>
2020-06-09 12:04:41 -0700

    Basic algebra problem generator done!

--- generate_problem.rb
+++ generate_problem.rb
@@ -9,3 +9,24 @@
 
 puts "Problem: x + 8 = 12"
 puts "Answer:  x = 4"
+
+# What do we do next? Options:
+# => Have the program generate a random variable (ex: 'a' instead of 'x')
+# => Have the program generate z and y, given that x = 4
+
+# Random variable first
+variable = ('a'..'z').to_a.sample # Choose random letter of the alphabet
+puts "Problem: #{variable} + 8 = 12"
+puts "Answer:  #{variable} = 4"
+
+# Choose random y and z, so that variable = 4
+# x + y = z, where x = 4
+x = 4
+# What is the minimum and maximum number you want for z?
+# => 0 to 25
+z = (0..25).to_a.sample
+# How do we calculate what y should be, given that x = 4 and z = random #
+y = z - x
+
+puts "Problem: #{variable} + #{y} = #{z}"
+puts "Answer:  #{variable} = #{x}"
